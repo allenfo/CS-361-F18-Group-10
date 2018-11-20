8c8e65abb9daa4dba550b9dbc9b009f2202688e6
votOSU <vot@oregonstate.edu>
2018-11-19 21:07:45 -0600

    Add files via upload

---
+++ Banks_Name.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Banks_Name (
+  ID int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  NAME varchar(255) NOT NULL
+) ENGINE=InnoDB;
+
+INSERT INTO Banks_Name
+(NAME)
+VALUES
+("Bank Of America"),
+("Capital One"),
+("Citi"),
+("JPMorgan Chase"),
+("Wells Fargo"); 
