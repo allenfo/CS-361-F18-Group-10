a39a02f8a43efca68039732dfc3c6f40761e6bb1
aseemprashar01 <31626866+aseemprashar01@users.noreply.github.com>
2018-11-21 10:15:58 -0800

    Update finapp.sql

--- finapp.sql
+++ finapp.sql
@@ -4,12 +4,9 @@ CREATE TABLE `fin_people` (
   `lname` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
-  `numacct` int(4) DEFAULT NULL,
   `annual_inc` int(20) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  KEY `numacct` (`finacct`),
-  FOREIGN KEY (`finacct`) REFERENCES `fin_acct` (`acct_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`user_id`)
+  )ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `fin_acct` (
@@ -27,13 +24,12 @@ CREATE TABLE `fin_acct_info` (
   `fin_year` int (4) NOT NULL DEFAULT '1980',
   `month_name` varchar(20) NOT NULL DEFAULT '0',
   `spending_food` int (11) NOT NULL DEFAULT '0',
-  `spending_housing`(11) NOT NULL DEFAULT '0',
-  `spending_insurance` (11) NOT NULL DEFAULT '0',
-  `spending_utilities` (11) NOT NULL DEFAULT '0',
-  `spending_medical` (11) NOT NULL DEFAULT '0',
-  `spending_misc` (11) NOT NULL DEFAULT '0'
-  PRIMARY KEY (`aid`,`uid`),
-  KEY `uid` (`uid`),
+  `spending_housing` int(11) NOT NULL DEFAULT '0',
+  `spending_insurance` int (11) NOT NULL DEFAULT '0',
+  `spending_utilities` int(11) NOT NULL DEFAULT '0',
+  `spending_medical` int(11) NOT NULL DEFAULT '0',
+  `spending_misc` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`aid`, `uid`),
   FOREIGN KEY (`aid`) REFERENCES `fin_acct` (`acct_id`),
   FOREIGN KEY (`uid`) REFERENCES `fin_people` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
